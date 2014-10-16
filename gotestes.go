33a7f4969909dfc0a1ca4709909d2fa89f58dcd5
Martin Bruse <zondolfin@gmail.com>
2014-10-16 12:49:59 +0200

    working

--- gotestes.go
+++ gotestes.go
@@ -9,22 +9,65 @@ import (
 	"os"
 	"os/exec"
 	"regexp"
+	"strings"
 )
 
 var testFileReg = regexp.MustCompile("^.*_test\\.go$")
+var testFuncReg = regexp.MustCompile("^Test")
 
-type visitor struct {
-	from string
-	to   string
+func isTestFunc(decl *ast.FuncDecl) bool {
+	if !testFuncReg.MatchString(decl.Name.Name) {
+		return false
+	}
+	if len(decl.Type.Params.List) != 1 {
+		return false
+	}
+	starExpr, ok := decl.Type.Params.List[0].Type.(*ast.StarExpr)
+	if !ok {
+		return false
+	}
+	selExpr, ok := starExpr.X.(*ast.SelectorExpr)
+	if !ok {
+		return false
+	}
+	ident, ok := selExpr.X.(*ast.Ident)
+	if !ok {
+		return false
+	}
+	return ident.Name == "testing" && selExpr.Sel.Name == "T"
 }
 
-func (self visitor) Visit(node ast.Node) (w ast.Visitor) {
+const (
+	searching = iota
+	running
+	last
+	done
+)
+
+type visitor struct {
+	state int
+	from  string
+	to    string
+	tests []string
+}
+
+func (self *visitor) Visit(node ast.Node) (w ast.Visitor) {
 	if fd, ok := node.(*ast.FuncDecl); ok {
-		if fd.Name.Name >= self.from && fd.Name.Name <= self.to {
-			cmd := exec.Command("go", "test", "-v", fmt.Sprintf("-test.run=^%v$", fd.Name.Name))
-			cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
-			if err := cmd.Run(); err != nil {
-				panic(err)
+		if isTestFunc(fd) {
+			switch self.state {
+			case searching:
+				if fd.Name.Name == self.from {
+					self.state = running
+				}
+			case running:
+				if fd.Name.Name == self.to {
+					self.state = last
+				}
+			case last:
+				self.state = done
+			}
+			if self.state == running || self.state == last {
+				self.tests = append(self.tests, fmt.Sprintf("(%v)", fd.Name.Name))
 			}
 		}
 	}
@@ -45,16 +88,23 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	v := &visitor{
+		from: *from,
+		to:   *to,
+	}
 	for _, child := range children {
 		if testFileReg.MatchString(child) {
 			f, err := parser.ParseFile(&token.FileSet{}, child, nil, 0)
 			if err != nil {
 				panic(err)
 			}
-			ast.Walk(visitor{
-				from: *from,
-				to:   *to,
-			}, f)
+			ast.Walk(v, f)
 		}
+	}
+
+	cmd := exec.Command("go", "test", "-v", fmt.Sprintf("-run=^%v$", strings.Join(v.tests, "|")))
+	cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
+	if err := cmd.Run(); err != nil {
+		panic(err)
 	}
 }
